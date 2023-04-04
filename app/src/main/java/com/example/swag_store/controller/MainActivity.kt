b9155e42f09f78682576acd9de54a9dab7586b01
Bhupender <kbhupender455@gmail.com>
2023-04-04 12:21:07 +0530

    first layout is done

--- app/src/main/java/com/example/swag_store/controller/MainActivity.kt
+++ app/src/main/java/com/example/swag_store/controller/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.swag_store.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.swag_store.R
+import com.example.swag_store.Services.DataService
+import com.example.swag_store.model.Category
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter:ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val categoryView = findViewById<ListView>(R.id.categoryListView)
+        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,DataService.category)
+        categoryView.adapter=adapter
     }
 }
